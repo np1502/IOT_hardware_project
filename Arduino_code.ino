f64aabc8d76838e272594dba4bf82f42fe13dac9
np1502 <44923662+np1502@users.noreply.github.com>
2020-11-07 15:07:37 +0530

    Update Arduino_code.ino

--- Arduino_code.ino
+++ Arduino_code.ino
@@ -1,9 +1,20 @@
+#include <SoftwareSerial.h> //imported library for serial communication
+#include<ArduinoJson.h> // imported json lib to send multiple data to node mcu
+SoftwareSerial s(5,6);//we shall consider pin 5 as Rx and pin 6 as Tx
+ 
 void setup() {
-  // put your setup code here, to run once:
-
+s.begin(9600);//To begin the serial communication between Arduino and NodeMCU with 9600 bits per second
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+ StaticJsonBuffer<100> jsonBuffer;
+ JsonObject& root=jsonBuffer.createObject();
+ root["voltage"] =analogRead(A0);
+ root["current"]=analogRead(A1);
+if(s.available()>0)
+{
+  root.printTo(s);//To print the JsonObject to the serial port
+}
+delay(100);
 }
